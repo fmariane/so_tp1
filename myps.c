18eff12fca21c8a5942bfb527dee7777cb3e1dc5
iagoadvaz <iagoadvaz@gmail.com>
2017-04-27 01:50:36 -0300

    Correcao myps e implementacao topzera

--- myps.c
+++ myps.c
@@ -1,20 +1,21 @@
 
-
 #include <stdio.h>
 #include <stdlib.h>
  
 
 
 int getChildren(char *children, int tab){
-	char file_name[50];
-   FILE *fp;
+	char file_name[50], proc_name[50], comm_name[50];
+   FILE *fp, *nf;
    int x,i;
  
 	
    snprintf(file_name, sizeof(file_name), "/proc/%s/task/%s/children", children, children); //seta o diretorio
+   snprintf(comm_name, sizeof(comm_name), "/proc/%s/task/%s/comm", children, children); //seta o diretorio
    fp = fopen(file_name,"r"); // abre arquivo para leitura
+   nf = fopen(comm_name,"r"); // abre arquivo para leitura
  
-   if( fp == NULL )
+   if(  fp == NULL || nf == NULL )
    {
       perror("Error while opening the file.\n");
       exit(EXIT_FAILURE);
@@ -22,10 +23,19 @@ int getChildren(char *children, int tab){
  
    while( ( fscanf(fp, "%s", children) ) != EOF ) //lê os filhos do processo
    {
+   	  snprintf(comm_name, sizeof(comm_name), "/proc/%s/task/%s/comm", children, children); //seta o diretorio
+   	  nf = fopen(comm_name,"r"); // abre arquivo para leitura
+   	  fscanf(nf, "%s", proc_name);
+
    	  for(i=0;i<tab;i++)
-   	  	printf("   ");
-      printf("%s\n",children);
-      snprintf(file_name, sizeof(file_name), "/proc/%s/task/%s/children", children, children);
+   	  	printf("	");
+
+      printf("%s\n",proc_name);
+
+      //snprintf(file_name, sizeof(file_name), "/proc/%s/task/%s/children", children, children);
+      //snprintf(comm_name, sizeof(comm_name), "/proc/%s/task/%s/comm", children, children); //seta o diretorio
+      fclose(nf);
+
        x = getChildren(children,tab+1);
    }
  
@@ -37,35 +47,28 @@ int getChildren(char *children, int tab){
 int main()
 {
    char children[10];
+   char file_name[50], proc_name[50], comm_name[50];
    int x, tab=1;
-   snprintf(children, sizeof(children), "1");   
-   printf("1\n");
+   FILE *fp, *nf;	
+   
+   fp = fopen("/proc/1/task/1/children","r"); // abre arquivo para leitura
+   nf = fopen("/proc/1/task/1/comm","r"); // abre arquivo para leitura
+
+   if(  fp == NULL || nf == NULL )
+   {
+      perror("Error while opening the file.\n");
+      exit(EXIT_FAILURE);
+   } 
+
+   snprintf(children, sizeof(children), "1");  
+
+   fscanf(nf, "%s", proc_name);
+   printf("%s\n",proc_name);
+
+   fclose(nf);
+   fclose(fp);
+
    x = getChildren(children,tab);
+
    return 0;
 }
-
-
-
-
-
-
-/*
-
-int main(void)
-{
-	char url[]="arquivo.txt",
-	     ch1, ch2, ch3;
-	FILE *arq;
-	
-	arq = fopen(url, "r");
-	if(arq == NULL)
-			printf("Erro, nao foi possivel abrir o arquivo\n");
-	else
-		while( (fscanf(arq,"%c %c %c\n", &ch1, &ch2, &ch3))!=EOF )
-			printf("%c %c %c\n", ch1, ch2, ch3);
-	
-	fclose(arq);
-	
-	return 0;
-}
-*/
---
+++ topzera.c
@@ -0,0 +1,103 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pwd.h>
+#include <sys/stat.h>
+
+
+
+int getChildren(char *children, int tab){
+	char file_name[50], proc_name[50], comm_name[50], stat_name[50], estado[2], trash[10];
+   FILE *fp, *nf, *st;
+   int x,i, pid;
+   struct passwd *pw ;
+ 
+	
+   snprintf(file_name, sizeof(file_name), "/proc/%s/task/%s/children", children, children); //seta o diretorio
+   snprintf(comm_name, sizeof(comm_name), "/proc/%s/task/%s/comm", children, children); //seta o diretorio
+   snprintf(stat_name, sizeof(stat_name), "/proc/%s/stat", children); //seta o diretorio
+   fp = fopen(file_name,"r"); // abre arquivo para leitura
+   nf = fopen(comm_name,"r"); // abre arquivo para leitura
+   st = fopen(stat_name,"r"); // abre arquivo para leitura
+ 
+   if(  fp == NULL || nf == NULL || st == NULL)
+   {
+      perror("Error while opening the file.\n");
+      exit(EXIT_FAILURE);
+   } 
+ 
+   while( ( fscanf(fp, "%s", children) ) != EOF ) //lê os filhos do processo
+   {
+	  snprintf(comm_name, sizeof(comm_name), "/proc/%s/task/%s/comm", children, children); //seta o diretorio
+     snprintf(stat_name, sizeof(stat_name), "/proc/%s/stat", children); //seta o diretorio
+	  nf = fopen(comm_name,"r"); // abre arquivo para leitura
+     st = fopen(stat_name,"r"); // abre arquivo para leitura
+
+	  fscanf(nf, "%s", proc_name);
+
+      for(i=0;i<=4;i++){ 
+         if(i==2)
+            fscanf(st, "%s", estado);
+         else if(i==3) fscanf(st, "%d", &pid);
+         else fscanf(st, "%s", trash);
+      } 
+
+      pw = getpwuid(pid);
+
+      printf("\n%s  |%s  |%s   |%s   |",children,pw->pw_name , proc_name, estado);
+      
+
+      snprintf(file_name, sizeof(file_name), "/proc/%s/task/%s/children", children, children);
+      snprintf(comm_name, sizeof(comm_name), "/proc/%s/task/%s/comm", children, children); //seta o diretorio
+      fclose(st);
+      fclose(nf);
+    x = getChildren(children,tab+1);
+   }
+ 
+   fclose(fp);
+   return 0;
+}
+
+
+int main()
+{
+   char children[10];
+   char file_name[50], proc_name[50], comm_name[50], estado[2], trash[10];
+   int x, tab=1,i, pid=0;
+   FILE *fp, *nf, *st;	
+   struct passwd *pw;
+   
+
+   printf("PID | User   | PROCNAME  | Estado |");
+
+
+   fp = fopen("/proc/1/task/1/children","r"); // abre arquivo para leitura
+   nf = fopen("/proc/1/task/1/comm","r"); // abre arquivo para leitura
+   st = fopen("/proc/1/stat","r"); // abre arquivo para leitura
+
+   if(  fp == NULL || nf == NULL || st == NULL)
+   {
+      perror("Error while opening the file.\n");
+      exit(EXIT_FAILURE);
+   } 
+
+   snprintf(children, sizeof(children), "1"); 
+   for(i=0;i<=4;i++){ 
+      if(i==2)
+         fscanf(st, "%s", estado);
+      else if(i==3) fscanf(st, "%d", &pid);
+      else fscanf(st, "%s", trash);
+   }
+
+   pw = getpwuid(pid);
+
+   fscanf(nf, "%s", proc_name);
+   printf("\n%s  |%s  |%s   |%s   |",children,pw->pw_name , proc_name, estado);
+
+   fclose(nf);
+   fclose(fp);
+
+   x = getChildren(children,tab);
+
+   return 0;
+}
